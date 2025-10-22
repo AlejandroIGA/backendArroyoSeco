1981b018a176588e582252782e165e52349f1cef
omarbasal18 <omarbasal18@gmail.com>
2025-10-22 12:23:41 -0600

    Se agrega crud funcional de user_profile

--- src/main/java/mx/edu/uteq/backend/model/UserProfile.java
+++ src/main/java/mx/edu/uteq/backend/model/UserProfile.java
@@ -14,8 +14,47 @@ public class UserProfile {
 
     @Column(name = "last_name")
     private String lastName;
-    
     private String cellphone;
     private String country;
-    
+
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
