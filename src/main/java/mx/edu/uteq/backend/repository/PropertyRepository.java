442419d05f197e322193ca4f2b0f595198f26595
CsCheems <j.dev.mx@outlook.com>
2025-11-01 16:52:07 -0600

    Obtencion de propiedades por id de dueño

--- src/main/java/mx/edu/uteq/backend/repository/PropertyRepository.java
+++ src/main/java/mx/edu/uteq/backend/repository/PropertyRepository.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import mx.edu.uteq.backend.model.Property;
+import java.util.List;
+
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
     Optional<Property> findByName(String name);
     Optional<Property> findById(Long id);
+    List<Property> findByOwnerId(Long ownerId);
 }
