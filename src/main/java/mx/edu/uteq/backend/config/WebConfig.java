48361bb87b340b2f5df690fe2a0879b6792a7411
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-11-04 16:07:53 -0600

    Cors para móvil

--- src/main/java/mx/edu/uteq/backend/config/WebConfig.java
+++ src/main/java/mx/edu/uteq/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:4173", "'http://localhost'")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:4173", "https://localhost")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
